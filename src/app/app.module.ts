37cac979a8aba7b08583fc803eaa262260244b47
Angel GMC <angel777pro12345@gmail.com>
2024-09-22 21:45:20 -0600

    [ADD] Creation of components for Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PorscheNewsComponent } from './porsche-news/porsche-news.component';
+import { AboutPorscheComponent } from './about-porsche/about-porsche.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PorscheNewsComponent,
+    AboutPorscheComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
